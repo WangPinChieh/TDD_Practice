87e1386cfacf7b8b2d2c011855026f5d6ac8893b
Jay <jay.wang@titansoft.com.sg>
2019-10-11 22:08:13 +0800

    add overall test

--- ConsoleApp1/FizzBuzz.cs
+++ ConsoleApp1/FizzBuzz.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
 {
 	public class FizzBuzz
 	{
-		public string processNumber(int i)
+		public string ProcessNumber(int i)
 		{
 			var result = string.Empty;
 			if (i % 3 == 0)
@@ -15,6 +15,17 @@ namespace ConsoleApp1
 				result += "Buzz";
 			if (string.IsNullOrEmpty(result))
 				result += i.ToString();
+
+			return result;
+		}
+
+		public List<string> Execute(List<int> input)
+		{
+			var result = new List<string>();
+			foreach (var v in input)
+			{
+				result.Add(ProcessNumber(v));
+			}
 
 			return result;
 		}
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using ConsoleApp1;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -9,49 +10,68 @@ namespace UnitTestProject1
 		[TestMethod]
 		public void ShouldProcessInput_1()
 		{
-			Assert.AreEqual("1", new FizzBuzz().processNumber(1));
+			Assert.AreEqual("1", new FizzBuzz().ProcessNumber(1));
 		}
 
 		[TestMethod]
 		public void ShouldProcessInput_2()
 		{
-			Assert.AreEqual("2", new FizzBuzz().processNumber(2));
+			Assert.AreEqual("2", new FizzBuzz().ProcessNumber(2));
 		}
 
 		[TestMethod]
 		public void ShouldProcessInput_3()
 		{
-			Assert.AreEqual("Fizz", new FizzBuzz().processNumber(3));
+			Assert.AreEqual("Fizz", new FizzBuzz().ProcessNumber(3));
 		}
 
 		[TestMethod]
 		public void ShouldProcessInput_5()
 		{
-			Assert.AreEqual("Buzz", new FizzBuzz().processNumber(5));
+			Assert.AreEqual("Buzz", new FizzBuzz().ProcessNumber(5));
 		}
 
 		[TestMethod]
 		public void ShouldProcessInput_6()
 		{
-			Assert.AreEqual("Fizz", new FizzBuzz().processNumber(6));
+			Assert.AreEqual("Fizz", new FizzBuzz().ProcessNumber(6));
 		}
 
 		[TestMethod]
 		public void ShouldProcessInput_10()
 		{
-			Assert.AreEqual("Buzz", new FizzBuzz().processNumber(10));
+			Assert.AreEqual("Buzz", new FizzBuzz().ProcessNumber(10));
 		}
 
 		[TestMethod]
 		public void ShouldProcessInput_15()
 		{
-			Assert.AreEqual("FizzBuzz", new FizzBuzz().processNumber(15));
+			Assert.AreEqual("FizzBuzz", new FizzBuzz().ProcessNumber(15));
 		}
 
 		[TestMethod]
 		public void ShouldProcessInput_30()
 		{
-			Assert.AreEqual("FizzBuzz", new FizzBuzz().processNumber(30));
+			Assert.AreEqual("FizzBuzz", new FizzBuzz().ProcessNumber(30));
+		}
+
+		[TestMethod]
+		public void ShouldProcessFizzBuzzCorrectly()
+		{
+			var input = new List<int>()
+			{
+				1, 2, 3, 5, 6, 10, 15, 30
+			};
+
+			var result = new FizzBuzz().Execute(input);
+
+			var expected = new List<string>()
+			{
+				"1", "2", "Fizz", "Buzz", "Fizz", "Buzz", "FizzBuzz", "FizzBuzz"
+			};
+
+			CollectionAssert.AreEqual(result, expected);
+
 		}
 	}
 }
